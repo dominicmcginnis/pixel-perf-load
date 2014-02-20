18ecc604afc819b66ad80982e19fe095785def7d
Dominic McGinnis <dominicmcginnis@gmail.com>
2014-02-20 11:04:48 -0800

    Altered to add a callback on timeout to demonstrate the ability to log when calls do timeout

--- test-pixel.js
+++ test-pixel.js
@@ -5,25 +5,29 @@ var fs = require("fs");
 http.createServer(function(request, response) {
  console.log("Recieving request: " + request.url);
 
-setTimeout(function() {
   if(request.url.indexOf('/testpixel.png') != -1) {
- 	fs.readFile("test.png", "binary", function(error, file) {
-	    if(error) {
-	      console.log("Error encountered");
-	      response.writeHead(500, {"Content-Type": "text/plain"});
-	      response.write(error + "\n");
-	      response.end();
-	    } else {
-	      console.log("Sending Response...");
-	      response.writeHead(200, {"Content-Type": "image/png"});
-	      response.write(file, "binary");
-	      response.end();
-	    }
-	  });
-
+		setTimeout(function() {
+		 	fs.readFile("test.png", "binary", function(error, file) {
+			    if(error) {
+			      console.log("Error encountered");
+			      response.writeHead(500, {"Content-Type": "text/plain"});
+			      response.write(error + "\n");
+			      response.end();
+			    } else {
+			      console.log("Sending Response...");
+			      response.writeHead(200, {"Content-Type": "image/png"});
+			      response.write(file, "binary");
+			      response.end();
+			    }
+			  });
+		}, 5000);
+	} else if (request.url.indexOf('/logtimeout') != -1) {
+		console.log("Logging timedout request: " + request.url);
+	    response.statusCode = 200;
+	    response.end();
 	} else {
-		console.log("Unrecognized path");
+		console.log("unknown request");
+	    response.statusCode = 404;
+	    response.end();
 	}
-}, 5000);
-
 }).listen(8888);
